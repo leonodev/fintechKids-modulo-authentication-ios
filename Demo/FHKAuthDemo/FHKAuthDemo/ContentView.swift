f4fa2e770be59a54e4d613e36be3ad60a6f05240
Fredy Leon <fredy.leon@ua.la.com>
2025-11-15 18:33:01 +0100

    initial commit

---
+++ Demo/FHKAuthDemo/FHKAuthDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FHKAuthDemo
+//
+//  Created by Fredy Leon on 15/11/25.
+//
+
+import SwiftUI
+import FHKAuth
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo/FHKAuthDemo/FHKAuthDemo/FHKAuthDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FHKAuthDemoApp.swift
+//  FHKAuthDemo
+//
+//  Created by Fredy Leon on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FHKAuthDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
