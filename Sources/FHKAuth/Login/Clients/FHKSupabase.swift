91b28eea0db371aada0194b2bf56a0be8bae0088
Fredy Leon <fredy.leon@ua.la.com>
2026-02-23 10:47:38 +0100

    remove unused comment

--- Sources/FHKAuth/Login/Clients/FHKSupabase.swift
+++ Sources/FHKAuth/Login/Clients/FHKSupabase.swift
@@ -25,14 +25,12 @@ public protocol FHKSupabaseProtocol: FHKInjectableProtocol {
 }
 
 public final class FHKSupabase: FHKSupabaseProtocol {
-   
-    private let servicesAPI: any ServicesAPIProtocol
-    //private let supabaseURL: URL
-    
-    //  // Properties injected safe
+    //  // Properties computed injected safe
     private var languageManager: any FHKLanguageManagerProtocol { inject.languageManager }
     private var configManager: any FHKConfigurationProtocol { inject.configManager }
-    
+   
+    private let servicesAPI: any ServicesAPIProtocol
+
     public init() {
         self.servicesAPI = inject.servicesAPI
     }
@@ -52,31 +50,6 @@ public final class FHKSupabase: FHKSupabaseProtocol {
             fatalError("FHK Error: Could not fetch URL")
         }
     }
-    
-//    public init() {
-//        let api = inject.servicesAPI
-//        self.servicesAPI = api
-//        
-//        let currentLanguageManager = inject.languageManager
-//        let currentConfigManager = inject.configManager
-//        
-//        do {
-//            let languageSelected = currentLanguageManager.selectedLanguage
-//            let languageType = currentLanguageManager.languageTypeFromCode(languageSelected)
-//            let environmentType = currentConfigManager.getEnvironment()
-//            
-//            let urlString = try api.getURL(environment: currentConfigManager.getEnvironment(),
-//                                           language: languageType,
-//                                           serviceKey: .supabase)
-//            guard let validURL = URL(string: urlString) else {
-//                fatalError("FHK Error: Supabase URL is not valid")
-//            }
-//            self.supabaseURL = validURL
-//            
-//        } catch {
-//            fatalError("FHK Error: Could not fetch URL from ServicesAPI")
-//        }
-//    }
 
     // MARK: - Login Authentication
     public func loginUser(email: String, password: String) async throws -> AuthResponseProtocol {
@@ -85,7 +58,6 @@ public final class FHKSupabase: FHKSupabaseProtocol {
             let session = try await getClient().auth.signIn(email: email, password: password)
             return SupabaseAuthResponse(session: session)
         } catch {
-            
             if let authError = error as? AuthError {
                 throw mapToDomainError(authError)
             }
@@ -134,27 +106,14 @@ public final class FHKSupabase: FHKSupabaseProtocol {
     }
     
     public func setSession(accessToken: String) async throws {
-        // Supabase necesita un Access Token para considerar que la sesión es válida.
-        // El Refresh Token se puede dejar vacío si solo quieres rehidratar la sesión actual.
         try await getClient().auth.setSession(accessToken: accessToken, refreshToken: "")
     }
 }
 
 public extension FHKSupabase {
-    
-//    public var client: SupabaseClient {
-//        do {
-//            let anonKey = try SecureKeyManager().getAnonKey()
-//            return SupabaseClient(supabaseURL: self.getSupabaseURL(), supabaseKey: anonKey)
-//        } catch {
-//            fatalError("FHK Error: Could not get Anon Key")
-//        }
-//    }
-    
+
     func getClient() async throws -> SupabaseClient {
         
-        // Pedimos los datos al MainActor (donde vive el LanguageManager)
-        // Solo "saltamos" al hilo principal para leer estas 2 variables
         let (langCode, env) = await MainActor.run {
             return (languageManager.selectedLanguage, configManager.getEnvironment())
         }
@@ -163,7 +122,6 @@ public extension FHKSupabase {
             languageManager.languageTypeFromCode(langCode)
         }
         
-        // Volvemos automáticamente al hilo de fondo para el resto
         let urlString = try servicesAPI.getURL(
             environment: env,
             language: languageType,
@@ -178,7 +136,6 @@ public extension FHKSupabase {
         guard let authError = error as? AuthError else { return nil }
         
         switch authError {
-            // API is in the client's auth response
         case .api(let message, let errorCode, let data, let response):
             
             return SupabaseApiError(
@@ -188,7 +145,6 @@ public extension FHKSupabase {
             )
             
         default:
-            // Handling other cases such as .unknown, .mock, etc.
             return nil
         }
     }
